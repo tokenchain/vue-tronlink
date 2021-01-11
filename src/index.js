b0e156f0921ac2e3ebefe1ece560f7294001f955
heskdx <jobhesk@gmail.com>
2021-01-11 18:56:19 +0800

    Please check!  4 files changed, 6 insertions(+), 5 deletions(-)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import NODES from "./utils/const"
 import TronLink from "./TronLink";
-import utilFullList from "./utils"
 import TronLinkComponent from "./mixins/vue-tronlink"
 import ImTokenComponent from "./mixins/vue-imtoken"
 
 export {
-    utilFullList,
     TronLink,
     NODES,
     ImTokenComponent,
